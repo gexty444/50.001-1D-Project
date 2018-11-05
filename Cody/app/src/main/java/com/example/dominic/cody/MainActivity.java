fd41dcc8cb2f43e6d7da5261ff69dd46ae6062cc
Dominic-C <dominiccordeiro@gmail.com>
2018-11-06 00:21:47 +0800

    camera improved preview resolution, but crashed upon hitting ok. Suspected missing permissions

--- Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
+++ Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.dominic.cody;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,9 +42,27 @@ public class MainActivity extends AppCompatActivity {
     private void dispatchTakePictureIntent()
     {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there is a camera activity to handle the intent
         if(takePictureIntent.resolveActivity(getPackageManager()) != null)
         {
-            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE); // launch an activity where we want the result when finished.
+            // create the file where the photo should go
+            // File class is an abstract representation of file and directory pathnames
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            if (photoFile != null)
+            {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.dominic.cody", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE); // launch an activity where we want the result when finished.
+
+            }
         }
     }
     @Override
@@ -46,5 +72,27 @@ public class MainActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             cameraImageView.setImageBitmap(imageBitmap);
         }
+    }
+
+    // creating a collision free storage solution for images
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException
+    {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); // Environment provides access to environment variables
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir); // args are prefix, suffix and directory
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
