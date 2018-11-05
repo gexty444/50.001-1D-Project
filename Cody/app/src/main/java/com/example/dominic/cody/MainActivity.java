6fd3860020bee2fb3aeb5889e6a406ec3bec6529
Dominic-C <dominiccordeiro@gmail.com>
2018-11-05 23:46:57 +0800

    camera takes picture, but low res preview

--- Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
+++ Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
@@ -1,28 +1,50 @@
 package com.example.dominic.cody;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     Button btn;
+    ImageView cameraImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.cameraButton);
+        cameraImageView = findViewById(R.id.cameraImageView);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-                startActivity(i);
+                dispatchTakePictureIntent();
             }
         });
+    }
+
+    private void dispatchTakePictureIntent()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE); // launch an activity where we want the result when finished.
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            cameraImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
