c21de5c73d9a333ba815fd5bf36a27890b7b9e56
Dominic-C <dominiccordeiro@gmail.com>
2018-11-08 23:22:10 +0800

    followed tutorial, boilerplate code

--- Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
+++ Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.dominic.cody;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // temporary buttons
+//    Button toWardrobe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void toWardrobe(View view) // called onClick from "to wardrobe" button
+    {
+        Intent intent = new Intent(this, WardrobeActivity.class); // intent calls another activity or application
+        startActivity(intent);
     }
 }
