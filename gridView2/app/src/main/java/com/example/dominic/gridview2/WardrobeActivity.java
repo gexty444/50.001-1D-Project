ab5892274b54828b5ae83983533372d4484e92be
Dominic-C <dominiccordeiro@gmail.com>
2018-11-14 22:55:43 +0800

    gridview is weird! but at least it kinda works.

---
+++ gridView2/app/src/main/java/com/example/dominic/gridview2/WardrobeActivity.java
@@ -0,0 +1,46 @@
+package com.example.dominic.gridview2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class WardrobeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wardrobe);
+
+        GridView gridview = findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this)); // without this, there was no images on this activity
+        gridview.setOnItemClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // detect if correct gridview is clicked, in this case, we only have 1 gridview
+        if(parent.getId() == R.id.gridview)
+        {
+            switch (position)
+            {
+//                case 0:
+//                {
+//                      // go to particular activity
+//                }
+                default:{
+                    Toast.makeText(getApplicationContext(), "No action associated with this item", Toast.LENGTH_LONG).show();
+                    break;
+                }
+            }
+        }
+
+    }
+}
