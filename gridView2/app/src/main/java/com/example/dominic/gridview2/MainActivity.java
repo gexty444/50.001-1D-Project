371d4941db3c0935ebddef2b4d6fecbdaa35d624
dominic <dominiccordeiro@gmail.com>
2018-11-16 11:33:59 +0800

    toolbar not working. try using floating button instead

--- gridView2/app/src/main/java/com/example/dominic/gridview2/MainActivity.java
+++ gridView2/app/src/main/java/com/example/dominic/gridview2/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.dominic.gridview2;
 
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +27,12 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // for options menu.
     setSupportActionBar(toolbar);
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
+
 }
