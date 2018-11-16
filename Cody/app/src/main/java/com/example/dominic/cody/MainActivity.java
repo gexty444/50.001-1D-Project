121b476287232168428a7f3bff71a32884b19513
thaddeusphua <thaddeus.phua@gmail.com>
2018-11-16 13:22:05 +0800

    start calendar by default

--- Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
+++ Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dominic.cody;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //starts calendar activity by default for debugging
+        Intent start_calendar = new Intent(this, Calendar.class);
+        startActivity(start_calendar);
     }
 }
