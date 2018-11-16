63d6c64b86760a9c641cf2858aaa8e64a714d423
thaddeusphua <thaddeus.phua@gmail.com>
2018-11-16 22:14:31 +0800

    selecting day on calendar gives you the OOTD

--- Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
+++ Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
@@ -2,21 +2,20 @@ package com.example.dominic.cody;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.CalendarView;
 import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
 
 public class Calendar extends AppCompatActivity {
 
     CalendarView calendarView;
     ImageView imageView;
     int imageResource;
+    int today;
     String TAG = "Logcat";
 
     @Override
@@ -26,19 +25,30 @@ public class Calendar extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        //developer defined widgets
         calendarView = findViewById(R.id.calendarView);
         imageView = findViewById(R.id.imageView);
-//        imageResource = 0;
+
+        //get today's date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMdd");
+        today = Integer.parseInt(dateFormat.format(calendarView.getDate()));
+
+        //this sets the imageView image to the OOTD taken today
+        imageResource = getResources().getIdentifier("@drawable/ootd" + today, null, ""+getApplicationContext().getPackageName());
+        Log.i(TAG,"imageResource: "+ imageResource);
+        imageView.setImageResource(imageResource);
+
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
                 String date = String.format("%d%02d%02d", year, month+1, dayOfMonth); //ISO 8601 compliant
-                Log.i(TAG, date); //Logcat for the date selected
+                Log.i(TAG, "Date selected: " +date); //Logcat for the date selected
 
-                imageResource = getResources().getIdentifier("@drawable/" + date, null, getApplicationContext().getPackageName());
-                Log.i(TAG,""+ imageResource);
+                //this sets the imageView image to a file with the date selected
+                imageResource = getResources().getIdentifier("@drawable/ootd" + date, null, ""+getApplicationContext().getPackageName());
+                Log.i(TAG,"imageResource: "+ imageResource);
+                imageView.setImageResource(imageResource);
             }
         });
     }
