3712b8c38d1ee2964b20bf5efe0cae9d11f601ec
thaddeusphua <thaddeus.phua@gmail.com>
2018-11-16 19:31:19 +0800

    stuff breaks after this

--- Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
+++ Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
@@ -1,13 +1,17 @@
 package com.example.dominic.cody;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
 
 public class Calendar extends AppCompatActivity {
+
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,7 @@ public class Calendar extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
     }
 
