eaf4ccae691f6653e86f9412b6a8c182ece95fae
thaddeusphua <thaddeus.phua@gmail.com>
2018-11-16 21:25:58 +0800

    Fixed month off by one bug

--- Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
+++ Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
@@ -2,6 +2,7 @@ package com.example.dominic.cody;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.ImageView;
 
 public class Calendar extends AppCompatActivity {
 
     CalendarView calendarView;
+    ImageView imageView;
+    int imageResource;
     String TAG = "Logcat";
 
     @Override
@@ -24,12 +28,17 @@ public class Calendar extends AppCompatActivity {
 
 
         calendarView = findViewById(R.id.calendarView);
+        imageView = findViewById(R.id.imageView);
+//        imageResource = 0;
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-                String date = String.format("%d%d%02d", year, month, dayOfMonth); //ISO 8601 compliant
+                String date = String.format("%d%02d%02d", year, month+1, dayOfMonth); //ISO 8601 compliant
                 Log.i(TAG, date); //Logcat for the date selected
+
+                imageResource = getResources().getIdentifier("@drawable/" + date, null, getApplicationContext().getPackageName());
+                Log.i(TAG,""+ imageResource);
             }
         });
     }
