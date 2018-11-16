ed8deba6e44d4fd91c236db026b3e30eb86c5c0b
thaddeusphua <thaddeus.phua@gmail.com>
2018-11-16 21:01:07 +0800

    Logcat for date selected

--- Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
+++ Cody/app/src/main/java/com/example/dominic/cody/Calendar.java
@@ -6,12 +6,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 
 public class Calendar extends AppCompatActivity {
 
     CalendarView calendarView;
+    String TAG = "Logcat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,15 @@ public class Calendar extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                String date = String.format("%d%d%02d", year, month, dayOfMonth); //ISO 8601 compliant
+                Log.i(TAG, date); //Logcat for the date selected
+            }
+        });
     }
 
 }
