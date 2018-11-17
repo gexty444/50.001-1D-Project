43162dbcdc02b73def44263b24e4f5c218b37ea5
huanan1 <huanan_cheng@mymail.sutd.edu.sg>
2018-11-18 01:58:11 +0800

    Add files via upload

---
+++ Cody/app/src/main/java/com/example/dominic/cody/Adapter.java
@@ -0,0 +1,58 @@
+package com.example.dominic.cody;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class Adapter extends PagerAdapter {
+
+    private List<Recommendations> recommendationsList;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public Adapter(List<Recommendations> recommendationsList, Context context) {
+        this.recommendationsList = recommendationsList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return recommendationsList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull //denotes that a parameter/field/method can never return null
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position){
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item, container, false);
+
+        ImageView imageView;
+        TextView title;
+
+        imageView = view.findViewById(R.id.image);
+        title = view.findViewById(R.id.title);
+
+        imageView.setImageResource(recommendationsList.get(position).getImage());
+        title.setText(recommendationsList.get(position).getTitle());
+
+        container.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.destroyItem(container, position, object);
+    }
+}
---
+++ Cody/app/src/main/java/com/example/dominic/cody/Recommendations.java
@@ -0,0 +1,34 @@
+package com.example.dominic.cody;
+
+public class Recommendations {
+    private int image;
+    private String title;
+
+    public Recommendations(int image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+
+    public Recommendations(int image) {
+        this.image = image;
+        this.title = "Default Title";
+    }
+
+    public int getImage(){
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
+
