d9b86557b214b31537a854f4289f6302588b5707
Dominic-C <dominiccordeiro@gmail.com>
2018-11-21 21:03:35 +0800

    merged gridview2 into Cody main app

--- Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
+++ Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.dominic.cody;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    // temporary buttons
-//    Button toWardrobe;
-
+    // boilerplate code to imitate home screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void toWardrobe(View view) // called onClick from "to wardrobe" button
-    {
-        Intent intent = new Intent(this, WardrobeActivity.class); // intent calls another activity or application
-        startActivity(intent);
-    }
+//    public void toWardrobe(View view)
+//    {
+//        Intent intent = new Intent(this, WardrobeActivity.class);
+//        startActivity(intent);
+//    }
 }
