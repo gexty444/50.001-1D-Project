3ee36dbf73f42ca7a238d8b8e9e6fa6c3d279eab
chiseng <thehedge96@hotmail.com>
2018-11-22 10:48:56 +0800

    files for cropper

--- Cody/app/proguard-rules.pro
+++ Cody/app/proguard-rules.pro
@@ -1,10 +1,10 @@
 # Add project specific ProGuard rules here.
 # You can control the set of applied configuration files using the
 # proguardFiles setting in build.gradle.
-#
+
 # For more details, see
 #   http://developer.android.com/guide/developing/tools/proguard.html
-
+-keep class android.support.v7.widget.** { *; }
 # If your project uses WebView with JS, uncomment the following
 # and specify the fully qualified class name to the JavaScript interface
 # class:
--- Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
+++ Cody/app/src/main/java/com/example/dominic/cody/MainActivity.java
@@ -1,111 +1,206 @@
 package com.example.dominic.cody;
 
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+//import com.theartofdev.edmodo.cropper.CropImage;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageActivity;
+import com.theartofdev.edmodo.cropper.CropImageOptions;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
+    static final int GALLERY_CRAWL = 3;
     Button btn;
+    Button picker;
+    private String filelocation = "";
+    public final String APP_TAG = "CodyClothes";
     ImageView cameraImageView;
+    File photoFile;
+    Uri mCropImageUri;
+    public final String TAG = "Logcat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.cameraButton);
+        picker = findViewById(R.id.PickPic);
         cameraImageView = findViewById(R.id.cameraImageView);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        picker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                CropImage.startPickImageActivity(MainActivity.this);
             }
         });
     }
 
-    private void dispatchTakePictureIntent()
-    {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there is a camera activity to handle the intent
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null)
-        {
-            // create the file where the photo should go
-            // File class is an abstract representation of file and directory pathnames
-            File photoFile = null;
-            try{
-                photoFile = createImageFile();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+    private void galleryCrawl() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, GALLERY_CRAWL);
 
-            if (photoFile != null)
-            {
-                Uri photoURI = FileProvider.getUriForFile(this, "com.example.dominic.cody", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE); // launch an activity where we want the result when finished.
-
-            }
-        }
     }
-    @Override
+
+    @SuppressLint("NewApi")
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            try{
-//                Bundle extras = data.getExtras();
-//                Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            Uri selectedImage = data.getData();
-//            Bitmap imageBitmap = getThumbnail(selectedImage);
-                Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
-                cameraImageView.setImageBitmap(imageBitmap);
+
+//        if (requestCode == GALLERY_CRAWL) {
+//            if (data != null) {
+//                try {
+//                    Uri imageUri = data.getData();
+//                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+//
+//                    Cursor cursor = getContentResolver().query(imageUri,
+//                            filePathColumn, null, null, null);
+//                    cursor.moveToFirst();
+//
+//                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//                    String picturePath = cursor.getString(columnIndex);
+//                    cursor.close();
+//
+//                    try {
+//
+//                        Bitmap rotatedBitMap = rotateBitmapOrientation(picturePath);
+//                        Bitmap scaledBitmap = Bitmap.createScaledBitmap(rotatedBitMap, 720, 576, false);
+//                        cameraImageView.setImageBitmap(scaledBitmap);
+//                    } catch (Exception e) {
+//                        // TODO Auto-generated catch block
+//                        e.printStackTrace();
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
 
 
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri imageUri = CropImage.getPickImageResultUri(this, data);
+
+            // For API >= 23 we need to check specifically that we have permissions to read external storage.
+            if (CropImage.isReadExternalStoragePermissionsRequired(this, imageUri)) {
+                // request permissions and handle the result in onRequestPermissionsResult()
+                mCropImageUri = imageUri;
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE);
+            } else {
+                // no permissions required or already granted, can start crop image activity
+                Intent intent = CropImage.activity(imageUri).getIntent(this);
+                startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
+
             }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
+
 
         }
     }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE) {
+            if (mCropImageUri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // required permissions granted, start crop image activity
+                startCropImageActivity(mCropImageUri);
+            } else {
+                Toast.makeText(this, "Cancelling, required permissions are not granted", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+    /*
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == CropImage.CAMERA_CAPTURE_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                CropImage.startPickImageActivity(this);
+            } else {
+                Toast.makeText(this, "Cancelling, required permissions are not granted", Toast.LENGTH_LONG).show();
+            }
+        }
+        if (requestCode == CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE) {
+            if (imageUri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // required permissions granted, start crop image activity
+                cropImage(imageUri);
+            } else {
+                Toast.makeText(this, "Cancelling, required permissions are not granted", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    */
 
     // creating a collision free storage solution for images
-    String mCurrentPhotoPath;
+//    String mCurrentPhotoPath;
 
-    private File createImageFile() throws IOException
-    {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); // Environment provides access to environment variables
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir); // args are prefix, suffix and directory
+//    private Fle createImageFile() throws IOException
+//    {
 
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+    //        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); // Environment provides access to environment variables
+//        File image = File.createTempFile(photoFileName, ".jpg", storageDir); // args are prefix, suffix and directory
+//
+//        mCurrentPhotoPath = image.getAbsolutePath();
+//        return image;
+//    }
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        // Read EXIF Data
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        // Return result
+        return rotatedBitmap;
     }
 
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
+    private void startCropImageActivity(Uri imageUri) {
+        CropImage.activity(imageUri)
+                .start(this);
+
+
     }
+
+
+//        public void onSelectImageClick (){
+//            CropImage.startPickImageActivity(this);
+//        }
 }
+
