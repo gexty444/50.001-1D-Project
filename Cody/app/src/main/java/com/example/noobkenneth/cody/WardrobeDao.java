25b2002af1f0b39b4c6383c7c842eed8a02f4451
Thaddeus <thaddeus.phua@gmail.com>
2018-11-28 21:46:33 +0800

    added insert delete all and select all from wardrobe entity

--- Cody/app/src/main/java/com/example/noobkenneth/cody/WardrobeDao.java
+++ Cody/app/src/main/java/com/example/noobkenneth/cody/WardrobeDao.java
@@ -1,7 +1,12 @@
 package com.example.noobkenneth.cody;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
 
 /**
  * This interface accesses the data from the database
@@ -11,7 +16,16 @@ import android.arch.persistence.room.Insert;
 @Dao
 public interface WardrobeDao {
 
-    @Insert
-    public void addWardrobeDb(WardrobeEntity wardrobeDb);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(WardrobeEntity wardrobeEntity);
+
+    @Query("DELETE FROM WARDROBE_ENTITY")
+    void deleteAll();
+
+    @Query("SELECT * FROM WARDROBE_ENTITY ORDER BY id desc")
+//  this method is for reading stuff without ViewModel
+//  List<WardrobeEntity> getAllClothes();
+
+    LiveData<List<WardrobeEntity>> getAllClothes();
 
 }
